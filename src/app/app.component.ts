5b30d754bfc562ff35e041b8e53ac3f0aa7ff1be
Kamal Raj <kamalrajganesan2000@gmail.com>
2024-10-03 15:49:06 +0530

    create the base that workd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SampleService } from './sample.service'
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'php-ng-ingtrn';
+
+  constructor(private http: HttpClient) {}
+
+  onClickk() {  
+    let data = {
+      name: "Kamalraj",
+      age: "24",
+      skill: "techie"
+    }
+    console.log(this.http.post("https://test.blackitechs.in/getContacts/getContacts.php", data).subscribe((val: any) => {
+      console.log("Value :", val)
+    }));
+  }
+
 }
